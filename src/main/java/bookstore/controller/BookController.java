74dba56782acd71c118615c056e83543ec198a4c
Takeit <larsubivec@gmail.com>
2025-06-11 15:07:50 +0400

    implemented CustomGlobalExceptionHandler and Bean Validation API

--- src/main/java/bookstore/controller/BookController.java
+++ src/main/java/bookstore/controller/BookController.java
@@ -4,6 +4,7 @@ import bookstore.dto.BookDto;
 import bookstore.dto.BookSearchParametersDto;
 import bookstore.dto.CreateBookRequestDto;
 import bookstore.service.BookService;
+import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -39,14 +40,14 @@ public class BookController {
     }
 
     @PostMapping
-    public BookDto createBook(@RequestBody CreateBookRequestDto bookDto) {
+    public BookDto createBook(@RequestBody @Valid CreateBookRequestDto bookDto) {
         return bookService.save(bookDto);
     }
 
     @ResponseStatus(HttpStatus.OK)
     @PutMapping("/{id}")
     public BookDto updateBookById(@PathVariable Long id,
-                                  @RequestBody CreateBookRequestDto bookDto) {
+                                  @RequestBody @Valid CreateBookRequestDto bookDto) {
         return bookService.update(id, bookDto);
     }
 
--- src/main/java/bookstore/dto/CreateBookRequestDto.java
+++ src/main/java/bookstore/dto/CreateBookRequestDto.java
@@ -1,13 +1,20 @@
 package bookstore.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
 public class CreateBookRequestDto {
+    @NotBlank
     private String title;
+    @NotBlank
     private String author;
+    @Pattern(regexp = "^\\d{13}$")
     private String isbn;
+    @Positive
     private BigDecimal price;
     private String description;
     private String coverImage;
