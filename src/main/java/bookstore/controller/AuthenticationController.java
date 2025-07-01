b2edbf0e6cf85d4016bd0a8c5b6af79b527dd285
Takeit <larsubivec@gmail.com>
2025-07-01 13:34:42 +0400

    added swagger documentation and constants

--- src/main/java/bookstore/controller/AuthenticationController.java
+++ src/main/java/bookstore/controller/AuthenticationController.java
@@ -7,6 +7,8 @@ import bookstore.dto.user.UserResponseDto;
 import bookstore.exception.RegistrationException;
 import bookstore.service.security.AuthenticationService;
 import bookstore.service.user.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +16,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Authentication management",
+        description = "Endpoints for managing user authentication")
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
@@ -21,12 +25,16 @@ public class AuthenticationController {
     private final UserService userService;
     private final AuthenticationService authenticationService;
 
+    @Operation(summary = "User Registration",
+            description = "Endpoint for registering new users")
     @PostMapping("/registration")
     public UserResponseDto register(@RequestBody @Valid UserRegistrationRequestDto requestDto)
             throws RegistrationException {
         return userService.register(requestDto);
     }
 
+    @Operation(summary = "User Login",
+            description = "Endpoint for authenticating an existing user")
     @PostMapping("/login")
     public UserLoginResponseDto login(@RequestBody @Valid UserLoginRequestDto request) {
         return authenticationService.authenticate(request);
