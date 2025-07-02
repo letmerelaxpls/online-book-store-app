18828ce91aa620b21ad2e5982601365d8abf1647
Takeit <larsubivec@gmail.com>
2025-07-02 12:22:51 +0400

    fixed CreateBookRequestDto

--- src/main/java/bookstore/dto/book/CreateBookRequestDto.java
+++ src/main/java/bookstore/dto/book/CreateBookRequestDto.java
@@ -1,6 +1,7 @@
 package bookstore.dto.book;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
@@ -22,5 +23,6 @@ public class CreateBookRequestDto {
     private BigDecimal price;
     private String description;
     private String coverImage;
+    @NotEmpty
     private List<Long> categoryIds;
 }
